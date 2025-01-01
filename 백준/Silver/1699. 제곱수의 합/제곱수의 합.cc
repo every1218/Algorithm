a26ec72d0eaec58aa8f9cfd995641285ae1d81ec
pengB1 <108857062+pengB1@users.noreply.github.com>
2025-01-01 12:50:03 +0900

    [Silver II] Title: 제곱수의 합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -1,41 +1,28 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-int dp[100001] ={0, 1};
+int dp[100001];
+
 int main() {
-    int num;
-    cin>>num;
+    int n;
+    cin>>n;
     
-    for(int i=1; i<=num; i++){
-        dp[i] = i;
-        if(sqrt(i) - (int)(sqrt(i)) == 0){
-            dp[i] = 1;
-        }
-        else{
-            for(int j=1; j*j<=i; j++){
-                dp[i] = min(dp[i], dp[i-j*j] + 1);
-            }
+    fill(dp,dp+100001,INT_MAX);
+    
+    dp[0]=0;
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j*j<=i; j++){ 
+            dp[i] = min(dp[i], dp[i-j*j]+1);
         }
     }
+
+    cout<<dp[n];
     
-    cout<<dp[num];
+    //dp[i] = min(dp[i], dp[i-j*j]+1);
+    
+    //ex) dp[13] 
+    //dp[13-1] + 1 = d[12] + dp[1] = 4 + 1 = 5
+    //dp[13-4] + 1 = d[9] + dp[4] = 1 + 1 = 2 <-당선 
+    //dp[13-9] + 1 = d[5] + dp[9] = 2 + 1 = 3
 }
-
-
-// dp[1] = 1
-// dp[2] = 2
-// dp[3] = 3
-// dp[4] = 1
-// dp[5] = 2
-// dp[6] = 3
-// dp[7] = 4
-// dp[8] = 2 
-// dp[9] = 1 
-
-// 7 = 2^2 + 1+1+1
-
-// 11 = 3^2 +1 + 1
-
-// 126 = 81 + 36 + 9
-//       9^2 + 6^2 + 3^2
