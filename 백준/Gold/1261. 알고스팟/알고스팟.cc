a3c94ea7982301b506358ce9178487283d63972f
pengB1 <108857062+pengB1@users.noreply.github.com>
2025-02-13 15:00:20 +0900

    [Gold IV] Title: 알고스팟, Time: 4 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -3,23 +3,25 @@ using namespace std;
 #define X first 
 #define Y second 
 
-int board[100][100];
-int vis[100][100];
+int board[101][101];
+int vis[101][101];
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
+
 
 int main() {
     int n,m;
     cin>>m>>n;
     
     for(int i=0; i<n; i++){
-        string str; cin>>str;
+        string str;
+        cin>>str;
         for(int j=0; j<m; j++){
             board[i][j] = str[j]-'0';
         }
     }
     
-    fill(&vis[0][0],&vis[0][0]+100*100, 99999);
+    fill(&vis[0][0], &vis[100][101], INT_MAX);
     
     queue<pair<int,int>>q;
     q.push({0,0});
@@ -34,27 +36,23 @@ int main() {
             ny = cur.Y + dy[k];
             
             if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
-            // if(vis[nx][ny]<=vis[cur.X][cur.Y])continue;
+            if(vis[cur.X][cur.Y] >= vis[nx][ny]) continue;
             
-            int temp = vis[cur.X][cur.Y] + board[nx][ny];
-            
-            if(temp<vis[nx][ny]){
-                if(board[nx][ny] == 0){
-                    vis[nx][ny]=vis[cur.X][cur.Y];
-                    q.push({nx, ny});
-                }else{
-                    vis[nx][ny]=vis[cur.X][cur.Y]+1;
-                    q.push({nx, ny});
-                }
+            if (board[nx][ny]==0){
+                vis[nx][ny]=vis[cur.X][cur.Y];
+            }else if(board[nx][ny]==1) {
+                if(vis[cur.X][cur.Y]+1 >= vis[nx][ny]) continue;
+                vis[nx][ny]=vis[cur.X][cur.Y]+1;
             }
+            
+            q.push({nx, ny});
         }
     }
-    
     // for(int i=0; i<n; i++){
     //     for(int j=0; j<m; j++){
     //         cout<<vis[i][j]<<' ';
     //     }cout<<'\n';
     // }
-    cout<<vis[n-1][m-1];
     
+    cout<<vis[n-1][m-1];
 }
