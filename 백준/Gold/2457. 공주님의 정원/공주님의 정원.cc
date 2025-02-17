c70b9aa3946d34acd9d463311c6e1f56b995ddeb
pengB1 <108857062+pengB1@users.noreply.github.com>
2025-02-18 00:47:38 +0900

    [Gold III] Title: 공주님의 정원, Time: 96 ms, Memory: 3464 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -1,70 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 그럼 간단하게 말해서 지는 날이 늦으면 좋은 거잖아
-
-// 다음 꽃의 시작(6.10)이 현재 꽃의 끝(6.30)보다 같거나 작고,
-// 다음 꽃의 끝이(12.10) 현재 꽃의 끝(6.30)보다 크면 되는 거잖아.
-// 이 조건을 만족하는 가장 마지막 다음 꽃을 고르면 되네?
-
 struct day{
-    int sm; //start month
-    int sd; 
-    int em; //end month
-    int ed;
+    int sm, sd, em,ed;
 };
 
-bool temp(day a, day b){
+bool temp (day a, day b){
     if(a.sm != b.sm) return a.sm<b.sm;
-    if (a.sd != b.sd) return a.sd<b.sd;
-    if (a.em != b.em) return a.em<b.em; 
+    if(a.sd != b.sd) return a.sd<b.sd;
+    if(a.em != b.em) return a.em<b.em;
     return a.ed<b.ed;
 }
 
-
 int main() {
-    int n; 
+    int n;
     cin>>n;
     
-    day d[100002];
+    day d[100001];
     
     for(int i=0; i<n; i++){
         cin>>d[i].sm>>d[i].sd>>d[i].em>>d[i].ed;
     }
     
+    //잠시만 sort함수로 정렬을 해야하나?
+    //일단 이전코드처럼 해보고 그다음 트라이
     sort(d, d+n, temp);
     
-    if(d[0].sm > 3 || d[0].sm==3 && d[0].sd>1){
+    
+    //이거 없어도 되지않나?
+    if(d[0].sm>3 || (d[0].sm==3 && d[0].sd>1)){
         cout<<0<<'\n';
         return 0;
     }
     
-    day p = {0,0,3,1}; //present
-    int flower=0;
-    int index=0;
+    day p = {0,0,3,1};
+    
+    int index=0, flower=0;
     
     while(p.em<12){
-        int last_index = -1;
-        int max_em = p.em, max_ed = p.ed;
+        int mx_index=-1;
+        int mx_em = p.em, mx_ed = p.ed; 
         
-        for (int i = index; i < n; i++) {
-            if (d[i].sm > p.em || (d[i].sm == p.em && d[i].sd > p.ed)) break;
-            if (d[i].em > max_em || (d[i].em == max_em && d[i].ed > max_ed)) {
-                last_index = i;
-                max_em = d[i].em;
-                max_ed = d[i].ed;
+        for(int i=index; i<n; i++){
+            // 1. d[i].s보단 크거나 같고
+            // 2. d[i].e보단 작아야함
+            if(p.em>d[i].sm || (p.em==d[i].sm && p.ed>=d[i].sd)){
+                if(mx_em<d[i].em || (mx_em==d[i].em && mx_ed<d[i].ed)){
+                    mx_em = d[i].em;
+                    mx_ed = d[i].ed;
+                    mx_index=i; 
+                }
             }
         }
+        // cout<<"mx_em : "<<mx_em<<' '<<mx_ed<<'\n';
+        // cout<<"mx_index :"<<mx_index<<'\n'<<'\n';
         
-        if (last_index == -1) {
+       if(mx_index==-1){
             cout<<0<<'\n';
             return 0;
         }
         
-        p.em = d[last_index].em; 
-        p.ed = d[last_index].ed;
+        p = d[mx_index];
+        index = mx_index+1;
         flower++;
-        index=last_index+1;
     }
-    cout<<flower<<'\n';
+    
+    cout<<flower;
 }
