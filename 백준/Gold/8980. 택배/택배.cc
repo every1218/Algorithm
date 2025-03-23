b80c5028fd4975a0240d1063ec371ece8d8c08f6
E_very <108857062+every1218@users.noreply.github.com>
2025-03-23 11:39:28 +0900

    [Gold II] Title: 택배, Time: 16 ms, Memory: 2156 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.cc
+++ 백준/Gold/8980. 택배/택배.cc
@@ -2,95 +2,55 @@
 using namespace std;
 
 struct Truck{
-    int st;
-    int ed;
-    int num;
+    int st; // 출발 마을
+    int ed; // 도착 마을
+    int num; // 박스 개수
 };
+
+// 정렬 기준: 도착 마을 오름차순
 bool compare(Truck a, Truck b){
-    if(a.st==b.st && a.ed==b.ed)
-        return a.num<b.num;
-    if(a.st==b.st)
-        return a.ed<b.ed;
-    return a.st<b.st;
+    if(a.ed == b.ed)
+        return a.st < b.st;
+    return a.ed < b.ed;
 }
 
-
 int main() {
-    //일단 되는대로 다 담았다가 (ex 1-4 20 이런 거도)
-    //더 가까운 거 있으면 버리고 그거 담기 (ex 2-3 20)
-    
     int n, c, m;
-    cin>>n>>c>>m;
+    cin >> n >> c >> m;
     
-    Truck t[2001];
-    vector<Truck>v;
-    int size=0;
-    
-    for(int i=0; i<m; i++){
-        int x,y,z;
-        cin>>t[i].st>>t[i].ed>>t[i].num;
+    vector<Truck> t(m); // 트럭 정보 저장
+    vector<int> v(n+1, 0); // 각 마을에서 트럭이 실은 박스 수
+
+    for(int i = 0; i < m; i++){
+        cin >> t[i].st >> t[i].ed >> t[i].num;
     }
     
-    sort(t, t+m, compare);
+    sort(t.begin(), t.end(), compare);
     
-    // for(int i=0; i<m; i++){
-    //     cout<<t[i].st<<' '<<t[i].ed<<' '<<t[i].num<<'\n';
-    // }
-    
-    int ans=0;
-    for(int i=0; i<m; i++){
+    int ans = 0;
+    for(int i = 0; i < m; i++){
         // cout<<"i : "<<i<<'\n';
-        for(int j=v.size()-1; j>=0; j--){
-            if(t[i].st >= v[j].ed){
-                ans+=v[j].num;
-                size -= v[j].num;
-                v.erase(v.begin()+j);
+        bool loadable = true;
+       
+        for(int j=t[i].st; j < t[i].ed; j++){
+            t[i].num = min(t[i].num, c - v[j]);
+            // cout<<"     t[i].num:"<<t[i].num<<'\n';
+            if (t[i].num == 0) {
+                loadable = false;
+                break;
             }
         }
-        
-        
-        if(size<c){
-            if(size+t[i].num>c){
-                v.push_back({t[i].st, t[i].ed, c-size});
-                size = c;
-            }else{
-                size += t[i].num; 
-                v.push_back({t[i].st, t[i].ed, t[i].num});
-            }
-        } 
-        else if(size==c){
-            vector<int>del;
-            for(int j=0; j<v.size(); j++){
-                if(t[i].ed < v[j].ed){
-                    if(t[i].num>v[j].num){
-                        v.push_back({t[i].st, t[i].ed, v[j].num});
-                        t[i].num-=v[j].num;
-                        del.push_back(j);
-                    }
-                    else{ //t[i].num<=v[j].num
-                        v.push_back({t[i].st, t[i].ed, t[i].num});
-                        v[j].num-=t[i].num;
-                        t[i].num=0;
-                    }
-                }
-            }
-            for(int j=del.size()-1; j>=0 ; j--){
-                v.erase(v.begin()+del[j]);
-            }
+        if (loadable) {
+            ans += t[i].num;
+            for (int j = t[i].st; j < t[i].ed; j++) 
+                v[j] += t[i].num;
         }
         
-        // for(int i=0; i<v.size(); i++){
-        //     cout<<"    "<<v[i].st<<' '<<v[i].ed<<' '<<v[i].num<<'\n';
-        // }
-        // cout<<"    size :"<<size<<'\n';
-        // cout<<"    ans :"<<ans<<'\n';
+        // for(int j=1; j <= n; j++){
+        //     cout<<"     "<<v[j]<<' ';
+        // }cout<<'\n';
+        // cout<<"     ans:"<<ans<<'\n';
     }
     
-    for(int j=0; j<v.size(); j++){
-        ans+=v[j].num;
-    }
-    
-    cout<<ans<<'\n';
-    
-    
+    cout << ans << '\n';
 }
