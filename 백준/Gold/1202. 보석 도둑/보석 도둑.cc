eaefb875aa2359a31332806ab3c1b94dd27250df
E_very <108857062+every1218@users.noreply.github.com>
2025-03-24 23:32:27 +0900

    [Gold II] Title: 보석 도둑, Time: 424 ms, Memory: 10524 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -10,33 +10,35 @@ bool compare(pair<int, int> a, pair<int, int> b) {
 int main() {
     int n, k; // 보석 개수, 가방 개수
     cin>>n>>k;
-    multiset<int> bag; //가방 (멀티셋 - 자동정렬)
+    vector<int> bag; //가방
     vector<pair<int, int>> jew(n); //보석 벡터 (페어)
+    priority_queue<int> pq; //현재 가방 용량 >=보석 무게를 만족하는 우선순위큐
     
     
-    for(int i=0; i<n; i++){ 
+    for(int i=0; i<n; i++){
         cin>>jew[i].first>>jew[i].second;
     }
-    
-    //보석 벡터 : 가격기준으로 정렬
-    sort(jew.begin(), jew.end(), compare);
     
     for(int i=0; i<k; i++){
         int temp;
         cin>>temp;
-        bag.insert(temp);
+        bag.push_back(temp);
     }
     
-    long long ans=0;
+    sort(bag.begin(), bag.end()); // 가방 : 오름차순 정렬
+    sort(jew.begin(), jew.end()); //보석 :무게와 가격 중 가격기준으로 오름차순 정렬
     
-    for(auto i=jew.begin(); i<jew.end(); i++){
-        int weight = i->first;
-        int price = i->second;
-        
-        auto it = bag.lower_bound(weight); //보석의 무게보다 높은 첫번째 가방의 인덱스로 이동
-        if(it != bag.end()){              //=현재 보석을 담을 수 있는 가장 작은 가방 찾기
-            ans+=price;  //결과 += 보석의 가격
-            bag.erase(it); //담은 가방은 삭제
+    long long ans=0;
+    int idx=0; //보석 인덱스
+    
+    for(int i=0; i<k; i++){ //가방 
+        while(idx<n && bag[i] >= jew[idx].first){//현재 가방 용량 >= 보석 무게
+            pq.push(jew[idx].second); //우선순위 큐에 가격 넣기
+            idx++;
+        }
+        if(!pq.empty()){
+            ans += pq.top(); //우선순위 top = 가격이 제일 비싼 보석
+            pq.pop(); //top 삭제
         }
     }
     
